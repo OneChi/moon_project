dd86dbe7b7b0c00672fb77c1680e21b5d429e78c
OneChi <alexbvbv@yandex.ru>
2022-01-13 04:11:29 +0300

    tests

--- delivery-engine.py
+++ delivery-engine.py
@@ -284,17 +284,6 @@ class TestingProtocols(unittest.TestCase):
         update = UpdatePackage(**data)
         update.validate()
 
-    def test_isupper(self):
-        self.assertTrue("FOO".isupper())
-        self.assertFalse("Foo".isupper())
-
-    def test_split(self):
-        s = "hello world"
-        self.assertEqual(s.split(), ["hello", "world"])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-
 
 # Test package_update 4
 # data = {'action': 'update_preference', 'timestamp': '12321', 'recipient_id': 1, 'personal_package' : True}
